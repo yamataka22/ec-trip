e45b9d33efdada1dac80574da0d33f38ff4426a6
yamataka22 <yamataka22@gmail.com>
2017-10-18 14:05:36 +0900

    localeなどの初期設定

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,16 @@ Bundler.require(*Rails.groups)
 
 module EcTrip
   class Application < Rails::Application
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = 'Tokyo'
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.default_locale = :ja
+
+    config.generators do |g|
+      g.assets false
+      g.helper false
+      g.test_framework :rspec
+      g.controller_specs false
+      g.view_specs false
+    end
   end
 end
