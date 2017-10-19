f582099189708eb4f1f893b10ccc58d386496400
yamataka22 <yamataka22@gmail.com>
2017-10-19 17:11:27 +0900

    現在の消費税取得ロジック追加

--- app/models/tax.rb
+++ app/models/tax.rb
@@ -1,2 +1,12 @@
 class Tax < ApplicationRecord
+  @@tax = nil
+  def self.now
+    if @@tax && @@last_date == Date.today
+      @@tax
+    else
+      @@tax = Tax.where('start_date <= ?', Date.today).order(:start_date).last.rate.to_f * 0.01 + 1
+      @@last_date = Date.today
+      @@tax
+    end
+  end
 end
