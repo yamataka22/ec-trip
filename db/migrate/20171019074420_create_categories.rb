29158d0e9dc8a441af37e492c66db57b74ccc727
yamataka22 <yamataka22@gmail.com>
2017-10-19 16:45:31 +0900

    categoryモデル作成

---
+++ db/migrate/20171019074420_create_categories.rb
@@ -0,0 +1,10 @@
+class CreateCategories < ActiveRecord::Migration[5.0]
+  def change
+    create_table :categories do |t|
+      t.string :name, null: false
+      t.integer :sequence, null: false, default: 0
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/categories.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :category do
+    name ""
+    sequence 1
+  end
+end
