70571f456cdfdf86dcb07c722285a4eeabc9dae8
yamataka22 <yamataka22@gmail.com>
2017-10-19 17:20:56 +0900

    itemの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,10 +9,7 @@ class Item < ApplicationRecord
   validates :price, numericality: { only_integer: true, greater_than: 1 }, allow_nil: true
 
   validates :description, presence: true, on: :publish
-  validates :content, presence: true, on: :publish
+  validates :about, presence: true, on: :publish
   validates :stock, presence: true, on: :publish
   validates :price, presence: true, on: :publish
-
-  scope :published, -> { where(status: [:selling, :end_of_sell]) }
-  scope :selling, -> { where(status: :selling) }
 end
