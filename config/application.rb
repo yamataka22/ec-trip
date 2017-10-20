3dd1630a3c3903322c9b172733e6090e32e431c2
yamataka22 <yamataka22@gmail.com>
2017-10-20 11:02:05 +0900

    Admin ログイン機能実装

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module EcTrip
   class Application < Rails::Application
     config.time_zone = 'Tokyo'
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     config.i18n.default_locale = :ja
 
     config.generators do |g|
---
+++ db/seeds/managers.rb
@@ -0,0 +1,1 @@
+Manager.create(email: 'admin@ec-trip.site', password: 'password', last_name: '山田', first_name: '太郎', admin: true)
