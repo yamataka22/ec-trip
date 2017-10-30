ab548598f50c5d696daee054eebe91df45fbf954
yamataka22 <yamataka22@gmail.com>
2017-10-30 09:57:41 +0900

    トップページのレイアウトを調整

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,8 +3,8 @@ class TopController < FrontBase
     @items = {}
     @items[:pickup] = Item.where(pickup: true).order(id: :desc).limit(6)
     @items[:new] = Item.order(id: :desc).limit(6)
-    @favorites = current_member.favorites
-    @topics = Topic.order(created_at: :desc).limit(5)
+    @favorites = current_member.favorites if member_signed_in?
+    @topics = Topic.order(created_at: :desc).limit(3)
     render '/top'
   end
 end
