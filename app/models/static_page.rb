15304359f7837d43d48b668d406ccdf0218ac760
yamataka22 <yamataka22@gmail.com>
2017-11-01 18:19:15 +0900

    静的ページ管理でプライバシーポリシーなどのマスターページは削除不可とする

--- app/models/static_page.rb
+++ app/models/static_page.rb
@@ -7,10 +7,18 @@ class StaticPage < ApplicationRecord
 
   before_validation :set_tinymce_images_path if Rails.env.development?
 
+  before_destroy :can_destroy?
+
   attr_accessor :preview
 
   private
   def set_tinymce_images_path
     TinymceConcern.set_images_path(self.content)
+  end
+
+  def can_destroy?
+    return true unless %w(about commercial privacy terms).include?(self.name)
+    errors.add :base, '対象のページは削除できません'
+    false
   end
 end
