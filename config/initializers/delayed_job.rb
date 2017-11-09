0758530b53e1a1995a22a2575439209d8a305a3f
yamataka22 <yamataka22@gmail.com>
2017-11-09 13:59:31 +0900

    delayedjobの定期的なupdateログの非表示化

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -4,3 +4,26 @@ Delayed::Worker.destroy_failed_jobs = false
 Delayed::Worker.max_attempts = 0
 # ログを分ける
 Delayed::Worker.logger =Logger.new(File.join(Rails.root,'log','delayed_job.log'))
+
+# 定期的なUPDATEログを非表示にする
+# https://github.com/collectiveidea/delayed_job/issues/477
+unless Rails.env.production?
+  module Delayed
+    module Backend
+      module ActiveRecord
+        class Job
+          class << self
+            alias_method :reserve_original, :reserve
+            def reserve(worker, max_run_time = Worker.max_run_time)
+              previous_level = ::ActiveRecord::Base.logger.level
+              ::ActiveRecord::Base.logger.level = Logger::WARN if previous_level < Logger::WARN
+              value = reserve_original(worker, max_run_time)
+              ::ActiveRecord::Base.logger.level = previous_level
+              value
+            end
+          end
+        end
+      end
+    end
+  end
+end
