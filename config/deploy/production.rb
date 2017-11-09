92440187773303adb51ab1fcfde54d81b7ce33e4
yamataka22 <yamataka22@gmail.com>
2017-11-09 23:56:03 +0900

    デプロイSSH名変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,6 +1,6 @@
 set :unicorn_rack_env, 'production'
 set :rails_env, 'production'
 
-role :web, %w{ec-trip-webapp1}
-role :app, %w{ec-trip-webapp1}
-role :db,  %w{ec-trip-webapp1}
+role :web, %w{ec-trip-pro-webapp1}
+role :app, %w{ec-trip-pro-webapp1}
+role :db,  %w{ec-trip-pro-webapp1}
